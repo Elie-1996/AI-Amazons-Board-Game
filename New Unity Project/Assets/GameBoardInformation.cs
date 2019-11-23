4cbb9611e6e3e9b60d8b67480be23ac8f6bb1d14
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-11-23 14:07:34 +0200

    Built UI and Logic Foundation
    * Game now loads the tiles and sets the camera in the correct position.
    * Added Material that will present what the tile represents.
    * Implemented essential logic data for the game.

---
+++ New Unity Project/Assets/GameBoardInformation.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+public enum Piece
+{
+    WHITEQUEEN,
+    BLACKQUEEN,
+    DESTROYEDTILE,
+    EMPTY
+}
+
+public enum MaterialIntensity
+{
+    LIGHT,
+    DARK
+}
+
+public class GameBoardInformation
+{
+    private Piece[,] board;
+    public int rows
+    {
+        get => board.GetLength(0);
+    }
+    public int columns
+    {
+        get => board.GetLength(1);
+    }
+
+    /* initializes an empty Board */
+    public GameBoardInformation(int rows, int columns)
+    {
+        if (rows <= 0 || columns <= 0)
+        {
+            throw new Exception("Could not initialize GameBoardInformation, rows and columns must be positive integers.");
+        }
+        board = new Piece[rows, columns];
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < columns; ++j)
+            {
+                board[i, j] = Piece.EMPTY;
+            }
+        }
+    }
+
+    public MaterialIntensity getPieceIntensity(int i, int j)
+    {
+        exceptionIfIndicesAreOutOfBounds(i, j, "getPieceIntensity");
+        if (i % 2 == 0)
+        {
+            return j % 2 == 0 ? MaterialIntensity.DARK : MaterialIntensity.LIGHT;
+        }
+        return j % 2 == 0 ? MaterialIntensity.LIGHT : MaterialIntensity.DARK;
+    }
+
+    public Piece getPieceAt(int i, int j)
+    {
+        exceptionIfIndicesAreOutOfBounds(i, j, "getPieceAt");
+        return board[i, j];
+    }
+
+    // TODO: Should involve which piece to burn.
+    public bool movePieceAndBurnLocation(int i, int j, int destination_i, int destination_j)
+    {
+        exceptionIfIndicesAreOutOfBounds(i, j, "movePiece (i,j)");
+        exceptionIfIndicesAreOutOfBounds(destination_i, destination_j, "movePiece (destination_i, destination_j)");
+        if (i == destination_i && j == destination_j) { return false; }
+        if (hasMovablePiece(i, j) == false) { return false; }
+        if (isMoveLegal(i, j, destination_i, destination_j)) { return false; }
+        
+        Piece piece = board[i, j];
+        board[i, j] = Piece.EMPTY;
+        board[destination_i, destination_j] = piece;
+        return true;
+    }
+
+    private bool isMoveLegal(int i, int j, int destination_i, int destination_j)
+    {
+        return isColumnMove(i, j, destination_i, destination_j) ||
+            isRowMove(i, j, destination_i, destination_j) ||
+            isDiagonalMove(i, j, destination_i, destination_j);
+    }
+
+    private bool isColumnMove(int i, int j, int destination_i, int destination_j)
+    {
+        return i == destination_i;
+    }
+
+    private bool isRowMove(int i, int j, int destination_i, int destination_j)
+    {
+        return j == destination_j;
+    }
+
+    private bool isDiagonalMove(int i, int j, int destination_i, int destination_j)
+    {
+        return Math.Abs(i - destination_i) == Math.Abs(j - destination_j);
+    }
+
+    private bool hasMovablePiece(int i, int j)
+    {
+        exceptionIfIndicesAreOutOfBounds(i, j, "hasMovablePiece");
+        return board[i, j] == Piece.WHITEQUEEN || board[i, j] == Piece.BLACKQUEEN;
+    }
+
+    // checks if [0,0] <= [i,j] < [m, n] 
+    private void exceptionIfIndicesAreOutOfBounds(int i, int j, string source)
+    {
+        if (i >= rows || i < 0 || j < 0 || j > columns)
+        {
+            throw new Exception(source + ": Input is out of Boundaries where boardSize is [" + rows + ", " + columns + "] and indices are [" + i + ", " + j + "].");
+        }
+    }
+}
---
+++ New Unity Project/Assets/GameLogic.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public Camera _camera;
+    public GameObject DarkTilePrefab;
+    public GameObject LightTilePrefab;
+
+    public Material DarkCubeMaterialEmpty;
+    public Material LightCubeMaterialEmpty;
+    public Material DarkTileFire;
+    public Material LightTileFire;
+    public Material DarkTileBlackQueen;
+    public Material LightTileBlackQueen;
+    public Material DarkTileWhiteQueen;
+    public Material LightTileWhiteQueen;
+
+    private GameBoardInformation info;
+    private GameObject[,] boardTiles;
+    private const float estimatedTileCameraSize = 0.6f;
+
+    // Start is executed once
+    void Start()
+    {
+        // TODO: rows and columns should be input later on
+        int rows = 10;
+        int columns = 10;
+
+        info = new GameBoardInformation(rows, columns);
+        generateAndPlaceTiles();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+
+    // TODO: Should involve which piece to burn.
+    private void movePiece(int i, int j, int destination_i, int destination_j)
+    {
+        bool didMove = info.movePieceAndBurnLocation(i, j, destination_i, destination_j);
+        if (didMove == false) return;
+        UpdateTileMaterial(i, j, info.getPieceAt(i,j));
+        UpdateTileMaterial(destination_i, destination_j, info.getPieceAt(destination_i, destination_j));
+    }
+
+    private void UpdateTileMaterial(int i, int j, Piece piece)
+    {
+        MaterialIntensity intensity = info.getPieceIntensity(i, j);
+
+        Material updatedMaterial = null;
+        if (intensity == MaterialIntensity.DARK)
+        {
+            switch (piece)
+            {
+                case Piece.BLACKQUEEN:
+                    updatedMaterial = DarkTileBlackQueen;
+                    break;
+                case Piece.DESTROYEDTILE:
+                    updatedMaterial = DarkTileFire;
+                    break;
+                case Piece.EMPTY:
+                    updatedMaterial = DarkCubeMaterialEmpty;
+                    break;
+                case Piece.WHITEQUEEN:
+                    updatedMaterial = DarkTileWhiteQueen;
+                    break;
+            }
+        }
+        else
+        {
+            switch (piece)
+            {
+                case Piece.BLACKQUEEN:
+                    updatedMaterial = LightTileBlackQueen;
+                    break;
+                case Piece.DESTROYEDTILE:
+                    updatedMaterial = LightTileFire;
+                    break;
+                case Piece.EMPTY:
+                    updatedMaterial = LightCubeMaterialEmpty;
+                    break;
+                case Piece.WHITEQUEEN:
+                    updatedMaterial = LightTileWhiteQueen;
+                    break;
+            }
+        }
+
+        boardTiles[i, j].GetComponent<Renderer>().material = updatedMaterial;
+        if (updatedMaterial == null)
+        {
+            Debug.LogError("GameLogic.cs, updateTileMaterial: updatedMaterial is null.");
+        }
+    }
+
+    private void generateAndPlaceTiles()
+    {
+        int rows = info.rows;
+        int columns = info.columns;
+        boardTiles = new GameObject[rows, columns];
+
+        int maximum = rows > columns ? rows : columns;
+        Vector3 cameraPosition = new Vector3(rows / 2.0f, -columns / 2.0f, -10.0f);
+        _camera.orthographicSize = estimatedTileCameraSize * maximum;
+        _camera.transform.position = cameraPosition;
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < columns; ++j)
+            {
+                GameObject newTile;
+                Vector3 tilePosition = new Vector3(i, -j, 0.0f);
+                if (info.getPieceIntensity(i, j) == MaterialIntensity.DARK)
+                {   
+                    newTile = Instantiate(DarkTilePrefab, tilePosition, Quaternion.identity, transform);
+                }
+                else
+                {
+                    newTile = Instantiate(LightTilePrefab, tilePosition, Quaternion.identity, transform);
+                }
+                newTile.AddComponent<BoxCollider>();
+                newTile.name = "Index (" + i + ", " + j + ")";
+                boardTiles[i, j] = newTile;
+            }
+        }
+    }
+}
